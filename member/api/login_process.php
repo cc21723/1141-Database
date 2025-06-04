55638cc93f7646c9d47bce2acd54b644163987e4
mackliu <macklun@gmail.com>
2025-06-04 14:37:37 +0800

    完成會員中心及登出功能

--- member/api/login_process.php
+++ member/api/login_process.php
@@ -6,16 +6,19 @@ $pdo=new PDO($dsn,'root','');
 /* echo "<pre>";
 print_r($_POST);
 echo "</pre>"; */
-$sql="SELECT * FROM `members` WHERE `acc`='{$_POST['acc']}' && `pw`='".md5($_POST['pw'])."'";
+//$sql="SELECT * FROM `members` WHERE `acc`='{$_POST['acc']}' && `pw`='".md5($_POST['pw'])."'";
+$sql="SELECT count(*) FROM `members` WHERE `acc`='{$_POST['acc']}' && `pw`='".md5($_POST['pw'])."'";
 //echo $sql;
-$mem=$pdo->query($sql)->fetch();
+//$mem=$pdo->query($sql)->fetch();
+$mem=$pdo->query($sql)->fetchColumn();
 
 
-if($mem['acc']==$_POST['acc'] && $mem['pw']==md5($_POST['pw'])){
+//if($mem['acc']==$_POST['acc'] && $mem['pw']==md5($_POST['pw'])){
+if($mem){
     //登入成功
     session_start();
-    $_SESSION['mem']=$mem;
-   header("location:../index.php");
+    $_SESSION['mem']=$_POST['acc'];
+   header("location:../member_center.php");
 
 }else{
     //登入失敗
